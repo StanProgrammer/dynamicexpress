e444a76c8419c82092dbec3714cfff41cdea14b9
AtibProgrammer <atibkhan392@gmail.com>
2023-03-02 18:04:47 +0530

    Designed the User and Product relationship

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete','root','atib', {
-    dialect: 'mysql',
-    host:'localhost'
-})
+const sequelize = new Sequelize('node-complete', 'root', 'atib', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
 
-module.exports=sequelize
+module.exports = sequelize;
